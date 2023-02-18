c445060deffc48b3899f63964a39089510ba4ebb
Tomas Mali <tomasmali@MacBook-Pro-di-Tomas.local>
2023-02-18 20:53:08 +0100

    some stuff

--- telepot/links_req.py
+++ telepot/links_req.py
@@ -2,69 +2,88 @@
 import requests
 from bs4 import BeautifulSoup
 
-#URL = "https://www.zara.com/it/it/woman-sale-l5503.html"
 
+from selenium import webdriver
+import time
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.chrome.options import Options
+
+
+
+
+
+#webdriver = webdriver.Chrome()
+
+
+options = Options()
+options.add_argument('--headless=new')
+options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+webdriver = webdriver.Chrome( chrome_options=options)
 
 def getLinks():
 
-
-
     cookies = {
-        'rid': '21fbe0fa-f2fa-4691-8120-75054458243e',
-        'storepath': 'it%2Fit',
-        'OptanonAlertBoxClosed': '2023-01-18T22:45:01.565Z',
-        'migrated': 'true',
-        'bm_sz': '4F81DEF957F0F1FB545D6C50EFC316EB~YAAQFmURAh21Jc6FAQAAU0jbzxI96PC/hVh0VMIiI9C+N+OViQ5QswoBM71jaR3eQ9Onj52MqcxCr2rKJz2Oc7FFDxGB87Nrf/YYJYNmClzt97H5vlnJIbF3xNn0ZF/q1aSp6yEOiAAWtbOp6pCxRzAdHSPCY3eJVjvoGSv8g80n7izFJBXYlUUrtDaW4ikj9zxUWLet2H62LchzCbjMTXbKQ+uRDoqNiVyeApzMuSEC/OVvvn1Hw4LV1xYDGzrYrnA0H4w94MCNI8EpM862JXiFo9Vwn6Ri+ARsuB3jB9nF~4273204~4538935',
-        'bm_mi': '061D1F0CEA3474EC36F692657C663E9C~YAAQFmURAjS1Jc6FAQAAzUnbzxLawp8+RLRTbt8ysrjzkZbyTxKSP0Ry3vlfWDDAMbKCDIrKDnUhR4JwRVQ9coLjrjViE2x93y705IhrjIHKVeyWAnizg+9W/s3Byl+LEYUSvucPTY2wKI/y0tzcUn58Vp6Wb53DVjIn5Zq+2LNuPzYMs2UUhanE8CmQ4x3Q/CG2Iv6biIqldG9JXbnNWCS3VJk0qfe+aUOwXphdLoR7CfmD2SkNkiBFu8foE6VqlZ9Y8B/e/FuRE9V3umSXdaKUyG99uofRnvOCfNwOE0i4GL92GccJ4hghdpC4TBU=~1',
-        'ITXSESSIONID': 'ad27220a9d834b6fa3a3ac280c8c4b0a',
-        'IDROSTA': '6bfb66adb6f9:1c60ef0fb755a418886f40c0b',
+        'bm_sz': 'F5AAA1262668072258F44D347A595209~YAAQtDYWAiIamS+GAQAAIZkZZBJYFsINWg6ASOa/RK5L09yIi8qVKei1YNqdLTaJYBxe4SqcL6VOOtNSsyZ4xyD2LaQZ7TXRrIESjK3S/aPltnEvCgMVv528d//RmQApg0zgWlEY8QxxXctrahQCPxSaJtndMykYXzbcqIGHLrTTc5dDJNXQ9k+k+6NTXlrs1b0YobLNjRFtoWjDJCtfWdhsnGiHVIm5v7r4zcIr4Hz78Q9vMU//To537brxl8hni69nw+LHEVeB1c1u735mj8DvM6LZI4sVr0YgoO7SuIeQ~3290418~3421489',
+        '_abck': 'CB824F1E384B196607D17E23FCD71E36~0~YAAQtDYWAi4amS+GAQAAEJ4ZZAlQOaVxksJ3sCtYMO0bVEzu2dGoYWOlzo2O9SE0yqrjQur7NFQsU1LTRjplIPLlBMaksNTc8L6SV6eEDIhBFBBzZx1llR3vCZEhNKpZYUsN2rV7x9PQQw8p1OlFbcsNV/PackURXEWIpg34r5U4gKuh+JAtZW72uuWbl513ZWrNFZkMwHk4sQYtDAr5WN6b5eYtzb8d+iVSFgJ4OZzCEFQY9UmpQMpkaea+UEOQvCHr2PEeyyUeA249+QICOpZ4xswQOuYSL0bS3FrjB7dHbxyXWNste5+n/FU1bFOdcsbbIpLJrlan2s9smJq5dliiKUyIke+osgdQfC+DRc9k9QGddBui24lQBdUOpks0KTw9dgy9076FjdVjjJoT5qxxfHuuzg==~-1~-1~-1',
+        'rid': 'fe3046c4-d35c-4ed1-b83a-d23baa7c5d52',
         'cart-was-updated-in-standard': 'true',
-        'OptanonConsent': 'isGpcEnabled=0&datestamp=Fri+Jan+20+2023+16%3A45%3A52+GMT%2B0100+(Ora+standard+dell%E2%80%99Europa+centrale)&version=6.28.0&isIABGlobal=false&hosts=&consentId=78e8f2ca-2010-49be-a2fe-125a232b071d&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1&geolocation=IT%3B34&AwaitingReconsent=false',
-        '_abck': '3E6AFB0791922E22A17904BBE6F2A5E1~-1~YAAQJmURAjB2GMOFAQAAdqTdzwmKXJBwsl6qxmagZdfTLc/QzCziVat86Sw5WGOk+pJYMIrudSh9ucDskeD8jYvab1FSrsL/Z+8OTyepjG8Ls4LJw3QMZgLTXEk8vhoPU0W6LLqz5vthDJDSYKYrCHeYflK+HiOytzNpEDnvi4aNrSzF5sfZ0C0Psm7ux2Xgf4/SnS3QXR05yv4XVrG4jULoNm70HYjMPjPPU+x49X9B6OWjYeTmXFOHZ/oMGUYz5l37CAC53CF0Q/r3vOjI9wn4eXy7oyQvfuWM2YxwUQAK5oajjCc3JN1P6qyCxwxS+NKu7ihEtljNeWp76VNYpKlUFHLPajAcR9M/k6RDvHqVWpRxwU1RGRrimJ1f3L62tp3VVhvsgs0wjPh70HH/Rdlqz/zi~0~-1~-1',
-        'ak_bmsc': 'DAF1D88845050C877F2745B43615FD61~000000000000000000000000000000~YAAQJmURAjl2GMOFAQAAYqXdzxKoD5eUbeRh9VTy/Mw6WtttbJhueJQA0w/ybektBX5W9fihSYu0j0N96I/H3XCebtApXOE5N38+qNxVyXGIailm3c/WjrdWZEHO0wENQNgZqPOm6FM0mEInP6aBghxmiFgfy+VE2R7T+rFiNVAd884O4BqnnfOpDIgnduWYLxcCD+tA6DfP0Vl/Ypyc4gn/PHs2MKAN+fXI4R+lC2WE/JpF3zqkjw+QXaX4nzQ0IM1OE5i7Bc+fGfJycPmb8Lc80cIeVtgDvItXy/HZnY/84wtE0dwc2sVnELVAUAmSuaB9MzH0KFIFRLNkyvxjkHGne0+8c/xa5lLhoIXuXToA52IFRhbff2hJ5a3EUgXab0Mt2f9a4LxMgwydtNqQ6GuimftoeAneqDPYI6EOI/XEa6ELm3AsbGl4G4MGXoAyK65bJw51jirXFyHftijnNdnHleLHZSLbkit4ydGSA91gjcAS2A==',
-        'bm_sv': 'F9EE7264D11CF28D68A6F03714B2FF0F~YAAQJmURAjp2GMOFAQAAYqXdzxItN945go7i2bjwgs35vnQMvbNZehYrnmME5oH6+bsMJyCx38QMOfuf70SiNCixkFPNJxDKoZnzUV0x0YpguAnXeyI9V7uH9VjmSNnhdkihBFIWMmPOltz8FtHUulYtHXcacLv0OcIQgRsXEWMwgq1wNtof84VXDuo5CpgqsUaBus0CO0C1lrAlSQQvW4PMy1HHqCDU1v3gAnX2O9aljaK6CVd6cPYx1kuPHY4=~1',
+        'ITXSESSIONID': '0dcea3afce672cd936c9255cd09ffcd3',
+        'OptanonAlertBoxClosed': '2023-02-18T10:37:38.302Z',
+        'selectedRegion': 'it',
+        'bm_mi': 'ABDFF901EA7CDBEB3971A49B5C9AB542~YAAQVzYWAnzAECiGAQAAcNMwZBJX8aVXyE0SBKrBH6pMJvrc/N3r2gQbM6MP9NO0CGM0PZ+r3XIsSsu2aP7aSI3eH/mAsNtPXLN6caO2rtzqTAxykKF6jhcaMsj7hF3LKE0BJW6vuXPkAMalHu0m6Exj0OCdd149JNzge0tc9F2dsmeG+z8jcbJ643/zF6jXIk9/mhXixR8WsuGuuw5BHhAQt3W2RDZLEPtDj4soQTyZgNu/I6StMJOve3rKanwP2M/T7S8pUZHs2BL9klpGtwhknOENyRZPGKKpLUNOSKOKpxTrpfWn+vmrro7Jz0wHJM1pSgRvj7bVfaV5HfOJFwhIGKT3I0E=~1',
+        'bm_sv': '959CEC0E6239254F242F39A3A4237331~YAAQVzYWApDAECiGAQAA0dkwZBIQ3H/8ywd0MxKBUEwKP5HbdRtgoE1czYQWiQsPtAYacfzg+WkIPxMNtyskCVRoD6W7rH6fghzSzaLU+P+hIfbNaZsfziekEEmSS22pxtqwRTBFAHXXyPgHGb1xkmuLOTCul0qnTd3Aw6JjTC43fA4ysE6k4XdOjgeDD5SjndehvJXSBPnOUwQdG8uj5tCWdKSK7d28d9iSEQaHaqE6qteBcfDTKwzjpeHhzig=~1',
+        'ak_bmsc': '09561AF7817160437542725ACDD857E9~000000000000000000000000000000~YAAQVzYWAr3AECiGAQAARRwxZBK6Mv8JM+WUIaDOkdgV0StlYBHA6eSqNvmhZBY1q9YrCcZj9eyxqJ3PpamVHiqC1qVk1OUIPxhDCIaOq/NvBkIoI/erY5prxZyXraoq/bTP+1MulZ5tAZcKX2Nn/yzjQ5wVK43qxE+A8o4qY/u8aBd/8NG695NqqeVI6rwrnijcCk39quBYcqUZMFAWJeDFG6QrLmrIyYer+3S3vZFxJkUToVak/xPhT5FbmVmzS5suWLZsQGTHmk/vixdwKaKG5gvcJtXuPcMZaibq45l+q1kWm/VTXG3T7eqFAy324OJ82YS3saYKRrfBaLu/4Ie9Qz81xvyNMKVkZ4dA2XFp3SCowafIlaLCRg48kdYZD2bKGyb4lz8JbIsYFLrHLUjXFvUGmTCci0mLVJXOTU6zsf8rKsxeYGI7F/XDlP09kAJHVlB4LIqERigITHw4eAjSeqIUOkqqpxox8OqH+NP8DG05Zz6ZlG7n6qRYnTGGfDUqBtqw',
+        'OptanonConsent': 'isGpcEnabled=0&datestamp=Sat+Feb+18+2023+12%3A31%3A03+GMT%2B0100+(Ora+standard+dell%E2%80%99Europa+centrale)&version=6.28.0&isIABGlobal=false&hosts=&consentId=295db455-ae01-4aaf-ba0b-fdd27c2ff01a&interactionCount=2&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1&geolocation=NL%3BNH&AwaitingReconsent=false',
+        'storepath': 'it%2Fit',
+        'IDROSTA': '7fabdd146b2f:1d840defea3942811c6faa5b2',
     }
 
     headers = {
         'authority': 'www.zara.com',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'accept-language': 'it-IT,it;q=0.9,la;q=0.8',
-        # 'cookie': 'rid=21fbe0fa-f2fa-4691-8120-75054458243e; storepath=it%2Fit; OptanonAlertBoxClosed=2023-01-18T22:45:01.565Z; migrated=true; bm_sz=4F81DEF957F0F1FB545D6C50EFC316EB~YAAQFmURAh21Jc6FAQAAU0jbzxI96PC/hVh0VMIiI9C+N+OViQ5QswoBM71jaR3eQ9Onj52MqcxCr2rKJz2Oc7FFDxGB87Nrf/YYJYNmClzt97H5vlnJIbF3xNn0ZF/q1aSp6yEOiAAWtbOp6pCxRzAdHSPCY3eJVjvoGSv8g80n7izFJBXYlUUrtDaW4ikj9zxUWLet2H62LchzCbjMTXbKQ+uRDoqNiVyeApzMuSEC/OVvvn1Hw4LV1xYDGzrYrnA0H4w94MCNI8EpM862JXiFo9Vwn6Ri+ARsuB3jB9nF~4273204~4538935; bm_mi=061D1F0CEA3474EC36F692657C663E9C~YAAQFmURAjS1Jc6FAQAAzUnbzxLawp8+RLRTbt8ysrjzkZbyTxKSP0Ry3vlfWDDAMbKCDIrKDnUhR4JwRVQ9coLjrjViE2x93y705IhrjIHKVeyWAnizg+9W/s3Byl+LEYUSvucPTY2wKI/y0tzcUn58Vp6Wb53DVjIn5Zq+2LNuPzYMs2UUhanE8CmQ4x3Q/CG2Iv6biIqldG9JXbnNWCS3VJk0qfe+aUOwXphdLoR7CfmD2SkNkiBFu8foE6VqlZ9Y8B/e/FuRE9V3umSXdaKUyG99uofRnvOCfNwOE0i4GL92GccJ4hghdpC4TBU=~1; ITXSESSIONID=ad27220a9d834b6fa3a3ac280c8c4b0a; IDROSTA=6bfb66adb6f9:1c60ef0fb755a418886f40c0b; cart-was-updated-in-standard=true; OptanonConsent=isGpcEnabled=0&datestamp=Fri+Jan+20+2023+16%3A45%3A52+GMT%2B0100+(Ora+standard+dell%E2%80%99Europa+centrale)&version=6.28.0&isIABGlobal=false&hosts=&consentId=78e8f2ca-2010-49be-a2fe-125a232b071d&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1&geolocation=IT%3B34&AwaitingReconsent=false; _abck=3E6AFB0791922E22A17904BBE6F2A5E1~-1~YAAQJmURAjB2GMOFAQAAdqTdzwmKXJBwsl6qxmagZdfTLc/QzCziVat86Sw5WGOk+pJYMIrudSh9ucDskeD8jYvab1FSrsL/Z+8OTyepjG8Ls4LJw3QMZgLTXEk8vhoPU0W6LLqz5vthDJDSYKYrCHeYflK+HiOytzNpEDnvi4aNrSzF5sfZ0C0Psm7ux2Xgf4/SnS3QXR05yv4XVrG4jULoNm70HYjMPjPPU+x49X9B6OWjYeTmXFOHZ/oMGUYz5l37CAC53CF0Q/r3vOjI9wn4eXy7oyQvfuWM2YxwUQAK5oajjCc3JN1P6qyCxwxS+NKu7ihEtljNeWp76VNYpKlUFHLPajAcR9M/k6RDvHqVWpRxwU1RGRrimJ1f3L62tp3VVhvsgs0wjPh70HH/Rdlqz/zi~0~-1~-1; ak_bmsc=DAF1D88845050C877F2745B43615FD61~000000000000000000000000000000~YAAQJmURAjl2GMOFAQAAYqXdzxKoD5eUbeRh9VTy/Mw6WtttbJhueJQA0w/ybektBX5W9fihSYu0j0N96I/H3XCebtApXOE5N38+qNxVyXGIailm3c/WjrdWZEHO0wENQNgZqPOm6FM0mEInP6aBghxmiFgfy+VE2R7T+rFiNVAd884O4BqnnfOpDIgnduWYLxcCD+tA6DfP0Vl/Ypyc4gn/PHs2MKAN+fXI4R+lC2WE/JpF3zqkjw+QXaX4nzQ0IM1OE5i7Bc+fGfJycPmb8Lc80cIeVtgDvItXy/HZnY/84wtE0dwc2sVnELVAUAmSuaB9MzH0KFIFRLNkyvxjkHGne0+8c/xa5lLhoIXuXToA52IFRhbff2hJ5a3EUgXab0Mt2f9a4LxMgwydtNqQ6GuimftoeAneqDPYI6EOI/XEa6ELm3AsbGl4G4MGXoAyK65bJw51jirXFyHftijnNdnHleLHZSLbkit4ydGSA91gjcAS2A==; bm_sv=F9EE7264D11CF28D68A6F03714B2FF0F~YAAQJmURAjp2GMOFAQAAYqXdzxItN945go7i2bjwgs35vnQMvbNZehYrnmME5oH6+bsMJyCx38QMOfuf70SiNCixkFPNJxDKoZnzUV0x0YpguAnXeyI9V7uH9VjmSNnhdkihBFIWMmPOltz8FtHUulYtHXcacLv0OcIQgRsXEWMwgq1wNtof84VXDuo5CpgqsUaBus0CO0C1lrAlSQQvW4PMy1HHqCDU1v3gAnX2O9aljaK6CVd6cPYx1kuPHY4=~1',
-        'referer': 'https://www.zara.com/it/it/donna-prezzi-speciali-l1314.html?v1=2105311',
-        'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+        'cache-control': 'max-age=0',
+        # 'cookie': 'bm_sz=F5AAA1262668072258F44D347A595209~YAAQtDYWAiIamS+GAQAAIZkZZBJYFsINWg6ASOa/RK5L09yIi8qVKei1YNqdLTaJYBxe4SqcL6VOOtNSsyZ4xyD2LaQZ7TXRrIESjK3S/aPltnEvCgMVv528d//RmQApg0zgWlEY8QxxXctrahQCPxSaJtndMykYXzbcqIGHLrTTc5dDJNXQ9k+k+6NTXlrs1b0YobLNjRFtoWjDJCtfWdhsnGiHVIm5v7r4zcIr4Hz78Q9vMU//To537brxl8hni69nw+LHEVeB1c1u735mj8DvM6LZI4sVr0YgoO7SuIeQ~3290418~3421489; _abck=CB824F1E384B196607D17E23FCD71E36~0~YAAQtDYWAi4amS+GAQAAEJ4ZZAlQOaVxksJ3sCtYMO0bVEzu2dGoYWOlzo2O9SE0yqrjQur7NFQsU1LTRjplIPLlBMaksNTc8L6SV6eEDIhBFBBzZx1llR3vCZEhNKpZYUsN2rV7x9PQQw8p1OlFbcsNV/PackURXEWIpg34r5U4gKuh+JAtZW72uuWbl513ZWrNFZkMwHk4sQYtDAr5WN6b5eYtzb8d+iVSFgJ4OZzCEFQY9UmpQMpkaea+UEOQvCHr2PEeyyUeA249+QICOpZ4xswQOuYSL0bS3FrjB7dHbxyXWNste5+n/FU1bFOdcsbbIpLJrlan2s9smJq5dliiKUyIke+osgdQfC+DRc9k9QGddBui24lQBdUOpks0KTw9dgy9076FjdVjjJoT5qxxfHuuzg==~-1~-1~-1; rid=fe3046c4-d35c-4ed1-b83a-d23baa7c5d52; cart-was-updated-in-standard=true; ITXSESSIONID=0dcea3afce672cd936c9255cd09ffcd3; OptanonAlertBoxClosed=2023-02-18T10:37:38.302Z; selectedRegion=it; bm_mi=ABDFF901EA7CDBEB3971A49B5C9AB542~YAAQVzYWAnzAECiGAQAAcNMwZBJX8aVXyE0SBKrBH6pMJvrc/N3r2gQbM6MP9NO0CGM0PZ+r3XIsSsu2aP7aSI3eH/mAsNtPXLN6caO2rtzqTAxykKF6jhcaMsj7hF3LKE0BJW6vuXPkAMalHu0m6Exj0OCdd149JNzge0tc9F2dsmeG+z8jcbJ643/zF6jXIk9/mhXixR8WsuGuuw5BHhAQt3W2RDZLEPtDj4soQTyZgNu/I6StMJOve3rKanwP2M/T7S8pUZHs2BL9klpGtwhknOENyRZPGKKpLUNOSKOKpxTrpfWn+vmrro7Jz0wHJM1pSgRvj7bVfaV5HfOJFwhIGKT3I0E=~1; bm_sv=959CEC0E6239254F242F39A3A4237331~YAAQVzYWApDAECiGAQAA0dkwZBIQ3H/8ywd0MxKBUEwKP5HbdRtgoE1czYQWiQsPtAYacfzg+WkIPxMNtyskCVRoD6W7rH6fghzSzaLU+P+hIfbNaZsfziekEEmSS22pxtqwRTBFAHXXyPgHGb1xkmuLOTCul0qnTd3Aw6JjTC43fA4ysE6k4XdOjgeDD5SjndehvJXSBPnOUwQdG8uj5tCWdKSK7d28d9iSEQaHaqE6qteBcfDTKwzjpeHhzig=~1; ak_bmsc=09561AF7817160437542725ACDD857E9~000000000000000000000000000000~YAAQVzYWAr3AECiGAQAARRwxZBK6Mv8JM+WUIaDOkdgV0StlYBHA6eSqNvmhZBY1q9YrCcZj9eyxqJ3PpamVHiqC1qVk1OUIPxhDCIaOq/NvBkIoI/erY5prxZyXraoq/bTP+1MulZ5tAZcKX2Nn/yzjQ5wVK43qxE+A8o4qY/u8aBd/8NG695NqqeVI6rwrnijcCk39quBYcqUZMFAWJeDFG6QrLmrIyYer+3S3vZFxJkUToVak/xPhT5FbmVmzS5suWLZsQGTHmk/vixdwKaKG5gvcJtXuPcMZaibq45l+q1kWm/VTXG3T7eqFAy324OJ82YS3saYKRrfBaLu/4Ie9Qz81xvyNMKVkZ4dA2XFp3SCowafIlaLCRg48kdYZD2bKGyb4lz8JbIsYFLrHLUjXFvUGmTCci0mLVJXOTU6zsf8rKsxeYGI7F/XDlP09kAJHVlB4LIqERigITHw4eAjSeqIUOkqqpxox8OqH+NP8DG05Zz6ZlG7n6qRYnTGGfDUqBtqw; OptanonConsent=isGpcEnabled=0&datestamp=Sat+Feb+18+2023+12%3A31%3A03+GMT%2B0100+(Ora+standard+dell%E2%80%99Europa+centrale)&version=6.28.0&isIABGlobal=false&hosts=&consentId=295db455-ae01-4aaf-ba0b-fdd27c2ff01a&interactionCount=2&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1&geolocation=NL%3BNH&AwaitingReconsent=false; storepath=it%2Fit; IDROSTA=7fabdd146b2f:1d840defea3942811c6faa5b2',
+        'if-none-match': 'W/"10885a-zVy/Bt41sg8ETuoMg2lpbum1Qa8"',
+        'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
         'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
+        'sec-ch-ua-platform': '"macOS"',
         'sec-fetch-dest': 'document',
         'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
         'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
     }
-
-    params = {
-        'v1': '2105311',
-    }
-
-
-
-
 
     links_list = []
 
+    webdriver.get('https://www.zara.com/it/it/donna-prezzi-speciali-l1314.html')
+    time.sleep(2)
 
-    for page_nr in range(10):
-        page = requests.get(
-            'https://www.zara.com/it/it/donna-prezzi-speciali-l1314.html',
-            params=params,
-            cookies=cookies,
-            headers=headers,
-)
-        # If no page is founded exit
-        if page.status_code != 200:
-            print(page.status_code)
+    elem = webdriver.find_element(By.ID, "onetrust-accept-btn-handler")
+    elem.send_keys(Keys.ENTER)       
+    webdriver.find_element(By.CLASS_NAME, "zds-button").send_keys(Keys.ENTER)
+
+    #  id="onetrust-accept-btn-handler"
+
+    for page_nr in range(1):
+
+        webdriver.get('https://www.zara.com/it/it/donna-prezzi-speciali-l1314.html')
+
+
+        #webdriver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        soup = BeautifulSoup(webdriver.page_source,"lxml")
+        print("###################")
+        # print(soup)
+
+        if not soup:
             break
 
-        soup = BeautifulSoup(page.content, "html.parser")
         ul_list = soup.find_all("ul", class_="product-grid__product-list")
         print(ul_list)
         for ul in ul_list:
@@ -80,16 +99,11 @@ def getLinks():
                     all_a = item.find_all('a', href=True)
                     for a in all_a:
                         links_list.append((a['href'], price_formated))
-                        # print(a['href'])
-
-            # print("\n")
-
 
     print(len(links_list))
     return links_list
 
-
-#getLinks()
+# getLinks()
 #print(getLinks()[0][1])
 
 
